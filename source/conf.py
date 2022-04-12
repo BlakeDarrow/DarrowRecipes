b4f95e82d68ef5cdf762cebdbb63a95e5e8e5b5e
BlakeDarrow <Blakerdarrow@gmail.com>
2022-04-12 16:53:29 -0500

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -3,6 +3,7 @@ import sphinx_rtd_theme
 
 extensions = [
     'sphinx_rtd_theme',
+    'sphinx.ext.githubpages',
 ]
 
 html_theme = "sphinx_rtd_theme"
@@ -19,3 +20,8 @@ exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
 
 html_show_sourcelink = False
+
+language = None
+html_static_path = ['_static']
+
+htmlhelp_basename = 'DarrowTools'
