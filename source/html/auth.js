a6c9dd9da95ff851009d8a71ea8831f5cc9fbcb3
BlakeDarrow <Blakerdarrow@gmail.com>
2022-06-21 14:48:14 -0500

    Restructure files

--- source/html/auth.js
+++ source/html/auth.js
@@ -12,7 +12,7 @@ const uiConfig = {
     },
   },
   signInFlow: "popup",
-  signInSuccessUrl: "Dashboard",
+  signInSuccessUrl: "dashboard",
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
